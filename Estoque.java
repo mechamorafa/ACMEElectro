c5822158194e0c7ca2f17f433bc6a6b9f6bcb87b
Rafael Ramos <rafaelramos@live.com>
2021-07-23 11:54:42 -0300

    Create Estoque.java

---
+++ Estoque.java
@@ -0,0 +1,23 @@
+public class Estoque {
+
+    public boolean cadastra (Eletronico eletronico){
+
+        return true;
+    }
+
+    public String busca (Eletronico[] eletronico){
+
+        return null;
+
+    }
+
+    public String buscaTudo (Eletronico[] eletronico){
+
+        return null;
+    }
+
+    public int pesquisaPreco (int preco) {
+        return 0;
+    }
+
+}
